ea25721320db5d5fec15ee1468983e19770a29ef
AdrianOlave <adrian@servitech.com.ar>
2023-04-07 13:21:20 -0300

    Hardcodear modal

--- client/src/components/ItemListContainer/ItemListContainer.jsx
+++ client/src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,38 +1,49 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react";
 import axios from "axios";
-import './ItemListContainer.css'
-import Item from './Item/Item'
-import LoadingContainer from '../LoadingContainer/LoadingContainer';
-import ItemModal from '../ItemModal/ItemModal';
+import "./ItemListContainer.css";
+import Item from "./Item/Item";
+import LoadingContainer from "../LoadingContainer/LoadingContainer";
+import ItemModal from "../ItemModal/ItemModal";
 
 const ItemListContainer = () => {
   const [productos, setProductos] = useState(null);
-  const [loading, setloading] = useState(true)
+  const [loading, setloading] = useState(true);
+
+  const [open, setOpen] = useState(true);
+
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   useEffect(() => {
-    axios.get('/productos')
+    axios
+      .get("/productos")
       .then((res) => {
         setProductos(res.data);
-        setloading(false)
+        setloading(false);
       })
       .catch((err) => {
         console.error(err);
       });
   }, []);
 
-
   return (
-    <div className='container-cards'>
-    <ItemModal />
+    <div className="container-cards">
+      <ItemModal open={open} onClose={handleClose} />
 
-      {loading ?
+      {loading ? (
         <LoadingContainer />
-        :
+      ) : (
         productos.map(({ id, nombre, codigo, descripcion }) => (
-          <Item key={id} nombre={nombre} codigo={id} descripcion={descripcion} />
+          <Item
+            key={id}
+            nombre={nombre}
+            codigo={id}
+            descripcion={descripcion}
+          />
         ))
-      }
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default ItemListContainer
+export default ItemListContainer;
