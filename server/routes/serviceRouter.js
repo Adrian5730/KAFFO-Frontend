487ba03bcb8812671a024181d7385f9987873c12
Adrian Olave <olaveves@gmail.com>
2023-05-03 18:43:26 -0300

    Delete server

--- server/routes/serviceRouter.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const serviceController = require('../controllers/serviceController')
-router.post('/payment',  serviceController.getPaymentLink);
-router.get('/subscription',  serviceController.getSubscriptionLink);
-
-module.exports = router;
