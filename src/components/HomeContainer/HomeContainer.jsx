b2eeaca4dbdaee7f45a86dc1b09c354e3656acc0
Adrian Olave <adrian@servitech.com.ar>
2023-07-08 12:31:43 -0300

    ADD LOGIN

--- src/components/HomeContainer/HomeContainer.jsx
+++ src/components/HomeContainer/HomeContainer.jsx
@@ -3,9 +3,15 @@ import ItemListContainer from "../ItemListContainer/ItemListContainer";
 import HeaderContainer from "../HeaderContainer/HeaderContainer";
 import ImageBack from "../ImageBack/ImageBack";
 import NavBar from "../NavBar/NavBar";
+import axios from "axios";
+import { useEffect } from "react";
 
 const HomeContainer = () => {
-  
+
+  useEffect(() => {
+    const user = axios.get("http://localhost:5050/user")
+    console.log(user)
+  })
   return (
     <div className="container">
       <ImageBack />
--- src/components/UserContainer/Login/Login.jsx
+++ src/components/UserContainer/Login/Login.jsx
@@ -1,31 +1,53 @@
-// import { FaGoogle } from 'react-icons/fa';
-import { Link } from 'react-router-dom'
-import './Login.css'
+import { FcGoogle } from "react-icons/fc";
+import { Link } from "react-router-dom";
+import "./Login.css";
+import { useEffect } from "react";
+import axios from "axios";
 const Login = () => {
-    return (
-        <>
-            <h1>LOGIN</h1>
-            <div className="container-form-login">
-                <form className='form-login' action="/login">
-                    <ul>
-                        <li>
-                            <span style={{ "--i": "#a955ff", "--j": "#ea51ff" }}>
-                                {/* <FaGoogle /> */}
-                            </span>
-                            <span className="titulo">Google</span>
-                        </li>
-                    </ul>
-                    <input type="text" placeholder='Correo electrónico' />
-                    <input type="password" placeholder='Contraseña' />
-                    <label className='message'> No tenes cuenta?</label>
-                    <Link to={'/user/register'} className="link_to link-user"><button className='btn-register'>Registrate</button></Link>
-                </form>
-            </div>
-            <div className='container-btn-login'>
-                <button className='btn-login'>Iniciar Sesión</button>
-            </div>
-        </>
-    )
-}
+  useEffect(() => {
+    axios
+      .get("http://localhost:5050/login/user", { withCredentials: true, credentials : "include" })
+      .then((response) => {
+        // Aquí puedes acceder a los datos de la sesión en response.data
+        console.log(response.data);
+        // ...
+      })
+      .catch((error) => {
+        // Manejo de errores
+        console.error(error);
+      });
+  });
 
-export default Login
+  return (
+    <>
+      <h1>LOGIN</h1>
+      <div className="container-form-login">
+        <form
+          className="form-login"
+          action="http://localhost:5050/login"
+          method="POST"
+        >
+          <ul>
+            <li>
+              <button className="google-button">
+                <FcGoogle className="google-icon" size={50} />
+                <span className="google-text">Iniciar sesión con Google</span>
+              </button>
+            </li>
+          </ul>
+          <input type="text" placeholder="Correo electrónico" />
+          <input type="password" placeholder="Contraseña" />
+          <label className="message"> No tenes cuenta?</label>
+          <Link to={"/user/register"} className="link_to link-user">
+            <button className="btn-register">Registrate</button>
+          </Link>
+        </form>
+      </div>
+      <div className="container-btn-login">
+        <button className="btn-login">Iniciar Sesión</button>
+      </div>
+    </>
+  );
+};
+
+export default Login;
